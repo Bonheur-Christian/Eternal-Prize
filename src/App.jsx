0dd19166693a79c4aa62755a101daafc7d712d9e
M.Bonheur <mbonheurc23@gmail.com>
2024-11-14 14:02:20 +0200

    New version of Eternal Prize

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import { BrowserRouter as Router, Route, Routes} from "react-router-dom";
+function App() {
+  return (
+    <>
+      <div className="text-2xl font-bold underline">Hello world</div>
+    </>
+  );
+}
+
+export default App;
